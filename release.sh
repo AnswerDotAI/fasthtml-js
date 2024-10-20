d6653a6de0d34d5f6648ddcf53c4ead264457401
Jeremy Howard <github@jhoward.fastmail.fm>
2024-10-20 15:19:56 +1000

    release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm version patch -m "Bump version to %s"
+npm version patch -m "Bump version to %s"  --tag-version-prefix=''
 NEW_VERSION=$(node -p "require('./package.json').version")
 git push origin $NEW_VERSION
 gh release create $NEW_VERSION --generate-notes
